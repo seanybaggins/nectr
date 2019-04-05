a2a1ca283f070a26287aad3f6ee43ca070f82e25
Adam Schafer <PseudoDesign@users.noreply.github.com>
2019-04-05 03:01:12 -0600

    building containers

--- docker/containers/rakefile.rb
+++ docker/containers/rakefile.rb
@@ -1,10 +1,21 @@
 def build_container(name)
-  
+  sh "docker build -t #{name} #{name}"
 end
 
 namespace :jenkins do
   desc "Build the Jenkins server"
   task :build do
-
+    build_container("nectr-jenkins")
   end
 end
+
+
+namespace :http_forwarding do
+  desc "Build the http-forwarding container"
+  task :build do
+    build_container("http-forwarding")
+  end
+end
+
+desc "Build all containers"
+task :build_all => ["http_forwarding:build", "jenkins:build"]
