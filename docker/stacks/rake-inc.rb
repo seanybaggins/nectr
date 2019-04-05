46192498eedb768a236017587695e98db5403930
Adam Schafer <PseudoDesign@users.noreply.github.com>
2019-04-05 03:43:50 -0600

    added stop

--- docker/stacks/rake-inc.rb
+++ docker/stacks/rake-inc.rb
@@ -1,11 +1,19 @@
 def launch_stack(name)
-  p = File.join(__dir__, name)
+  p = File.join(__dir__, "#{name}.yml")
   sh "docker stack deploy -c #{p} #{PROJECT_NAME}-#{name}"
+end
+
+def stop_stack(name)
+  sh "docker stack rm #{PROJECT_NAME}-#{name}"
 end
 
 namespace :local_http do
   desc "Launch the Jenkins container on localhost at port 8000"
   task :launch => ["docker:containers:jenkins:build"] do
     launch_stack("local-http")
+  end
+
+  task :stop do
+    stop_stack("local-http")
   end
 end
