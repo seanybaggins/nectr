c6e24f9fef87ccea1527dd61e04c3eb2c2bcc218
Adam Schafer <PseudoDesign@users.noreply.github.com>
2019-04-26 14:02:46 -0600

    updated key rake

--- keys/rake-inc.rb
+++ keys/rake-inc.rb
@@ -1,12 +1,19 @@
 namespace :local_ssl do
-  keyfile = File.join(__dir__, 'nginx.key')
-  crtfile = File.join(__dir__, 'nginx.crt')
+  local_ssl_dir = File.join(__dir__, 'local_ssl')
+  keyfile = File.join(local_ssl_dir, 'nginx.key')
+  crtfile = File.join(local_ssl_dir, 'nginx.crt')
   desc "Create a self-signed ssl cert"
   task :keygen do
-    if File.exist?(keyfile)
+    if File.exist?(local_ssl_dir)
       puts 'local_ssl certs already exist'
     else
+      mkdir local_ssl_dir
       sh "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout #{keyfile} -out #{crtfile}"
     end
+  end
+
+  desc "Clean self-signed ssl cert"
+  task :clean do
+    rm_rf local_ssl_dir
   end
 end
